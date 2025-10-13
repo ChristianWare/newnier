7c742e5318bc3e2fb2380fdb40bd36cda054dcca
Chris Ware <c.p.ware@hotmail.com>
2025-10-13 10:55:10 -0700

    About numbers done

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,11 @@
+import AboutNumbers from "@/components/AboutPage/AboutNumbers/AboutNumbers";
 import AboutPageIntro from "@/components/AboutPage/AboutPageIntro/AboutPageIntro";
 
 export default function AboutPage() {
   return (
     <main>
       <AboutPageIntro />
+      <AboutNumbers />
     </main>
   );
 }
